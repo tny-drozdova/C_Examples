e5095c3e1e075232f55bb08d9c25eb81c09bf9cb
Tanya Drozdova <tny.drozdova@gmail.com>
2023-06-14 21:57:42 -0400

    Added Examples009_IntroArray/ and Example010_MethodArray/ to the project

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,29 @@
+﻿/*How to determin the positions of the element we need:
+Имеется одномерный массив array из n элементов, требуется найти элемент массива, равный find
+1. Установить счетчик index в позицию 0
+2. Если array [index] = find, алгоритм завершил работу успешно.
+3. Увеличить index на 1
+4. Если index < n, то перейти к шагу 2. В противном случае алгоритм завершил работу безуспешно.*/
+
+
+
+int[] array = { 1, 12, 18, 31, 4, 18, 15, 16, 17, 18 };
+int n = array.Length; // Length returns the number of the elements in array
+int find = 18; // We will be looking for element 4
+
+int index = 0;  //Установить счетчик index в позицию 0
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; // index = index + 1;
+}
+
+
+
+
+
---
+++ Examples009_IntroArray/Program.cs
@@ -0,0 +1,29 @@
+﻿// For this method, we will take 3 numbers and will output the max. WE will do it for 9 numbers.
+// The method is MAX and the number type we want to be returned - int.
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1; //insider's result is where the arg1 will be stored (the max result at the end)
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//We need to define the array:
+// Our element's index always starts with 0:
+//              0   1   2   3   4   5   6   7   8
+int[] array = { 112, 21, 31, 41, 15, 61, 117, 18, 19 };
+
+// array[0] = 12; // To talk to our element and assign any number to it (in example below it's 12)
+// Console.WriteLine(array[4]); // Example how to talk to array and get it's value based on the index
+
+// How to find max of 9 numbers:
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
+
+
+
